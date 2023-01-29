68af9968a9bcb3c82b74c57cc6b8a96d4c8d1723
ABossOfMyself <abossofmyself@gmail.com>
2023-01-29 22:03:28 +0500

    initial commit

---
+++ packages/hardhat/deploy/00_deploy_streamer.js
@@ -0,0 +1,30 @@
+// deploy/00_deploy_streamer.js
+
+const { ethers } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("Streamer", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+
+  const streamer = await ethers.getContract("Streamer", deployer);
+
+  console.log("\n 🤹  Sending ownership to frontend address...\n");
+
+  const ownerTx = await streamer.transferOwnership(
+    "0x047821Dc2b13F680FeD9B006F0868bE43AcF4fe6"
+  );
+
+  console.log("\n       confirming...\n");
+  const ownershipResult = await ownerTx.wait();
+  if (ownershipResult) {
+    console.log("       ✅ ownership transferred successfully!\n");
+  }
+};
+
+module.exports.tags = ["Streamer"];
